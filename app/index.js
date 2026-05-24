6a69449c05bd58c56f0c3791f803282189433452
Joao Pires <academicaconta215@gmail.com>
2026-05-23 22:26:55 -0300

    Revert "Ajusta rota inicial do app MKP"
    This reverts commit 830f501090507a5241d58a24927312b37529d339.

--- app/index.js
+++ app/index.js
@@ -28,10 +28,6 @@ function requireAuth(req, res, next) {
   res.redirect("/mkp/login");
 }
 
-app.get("/", (req, res) => {
-  res.redirect("/mkp");
-});
-
 // Splash
 app.get("/mkp", (req, res) => {
   res.render("mkp/splash");
